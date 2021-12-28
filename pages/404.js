e88e6e017d53342d04f6bf0213f99ba655218b64
Chung Kao <chungckao@gmail.com>
2021-12-28 14:14:29 +0800

    Fix 404 page to include <Layout> and <Head> components

--- pages/404.js
+++ pages/404.js
@@ -1,1 +1,11 @@
-export const Custom404 = () => <h1>404 - Page Not Found</h1>;
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export const Custom404 = () => (
+  <Layout>
+    <Head>
+      <title>404 Page Not Found</title>
+    </Head>
+    <h1>404 - Page Not Found</h1>;
+  </Layout>
+);
